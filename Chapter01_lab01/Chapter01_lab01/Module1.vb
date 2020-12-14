89d7fe894a374ef2d542448f4625cd6f1fa7350b
Ale Suzuki <49762170+alesuzuki@users.noreply.github.com>
2020-12-14 20:27:29 -0300

    Add files via upload

---
+++ Chapter01_lab01/Chapter01_lab01/Module1.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+
+    Sub Main()
+        'Write code here...
+
+        'Show typed name and course
+        Console.WriteLine("Name: Alessandra")
+        Console.WriteLine("Course: VB.Net Module 1")
+
+        'Wait type of any key in prompt
+        Console.ReadLine()
+
+    End Sub
+
+End Module
---
+++ WindowsApplication02_Forms_and_Toolboxes/WindowsApplication02_Forms_and_Toolboxes/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+        'PROPERTY AutoScaleMode
+        'None = Disable automatic scaling / Don't let user alter form size
+        'Font = scale according with font size / Do let user alter form size
+        'Dpi = scale according monitor's resolution using inches / Do let user alter form size
+        'Inherit = scale according forms control acoording "AutoScaleMode from Father's class / Do let user alter form size
+        Me.AutoScaleMode = Windows.Forms.AutoScaleMode.Dpi
+
+
+
+    End Sub
+End Class
